331ef3b0353e38e85f56e528ba18abc26d76a252
Denish-M <denishmumbaiwala@gmail.com>
2013-09-29 16:27:06 -0700

    Test Classes

---
+++ src/experiments/wellbeing/CMDSimulator.java
@@ -0,0 +1,217 @@
+package experiments.wellbeing;
+
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Properties;
+/*
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+*/
+import massim.Agent;
+import massim.SimulationEngine;
+import massim.Team;
+import massim.TeamTask;
+import massim.agents.advancedactionmap.AdvActionMAPAgent;
+import massim.agents.advancedactionmap.AdvActionMAPRepAgent;
+import massim.agents.advancedactionmap.AdvActionMapTeam;
+import massim.agents.basicactionmap.BasicActionMAPAgent;
+import massim.agents.basicactionmap.BasicActionMAPTeam;
+import massim.agents.helperinitactionmap.HelperInitActionMAPAgent;
+import massim.agents.helperinitactionmap.HelperInitActionMAPTeam;
+import massim.agents.nohelp.NoHelpTeam;
+import massim.agents.reassignment.RAAgent;
+
+
+/**
+ * This is an experiment for testing wellbeing expression of agents
+ * over Resource Multiplier, Disturbance, No of Runs & Expression versions
+ * 
+ *   
+ * @author Denish M
+ *
+ */
+public class CMDSimulator {
+
+	static String strMsg = "";
+	public static void main(String[] args) {
+			try {
+				//sendEmail();
+				//runSimulation(10000, 2, 1);
+				//runSimulation(10000, 2, 2);
+				//runSimulation(5000, 2, 3);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+	/*
+	 * There are two versions of wellbeing.
+	 */
+	public static void runSimulation(int numberOfRuns, int impVersion, int experiment) throws Exception {
+	//	if(wbVersion < 1 || wbVersion > 2)
+		//	throw new Exception("Wellbeing value is invalid!");
+		if(numberOfRuns < 1)
+			throw new Exception("numberOfRuns is invalid!");
+		
+		SimulationEngine.colorRange = 
+			new int[] {0, 1, 2, 3, 4, 5};
+		SimulationEngine.numOfColors =  
+			SimulationEngine.colorRange.length;
+		SimulationEngine.actionCostsRange = 
+		new int[] {10, 40, 150, 200, 250, 300, 350, 500};	
+	
+		/* Create the teams involved in the simulation */
+		Team.teamSize = 8;
+		Team[] teams = new Team[3];		
+		teams[0] = new AdvActionMapTeam();
+		teams[1] = new HelperInitActionMAPTeam();
+		teams[2] = new BasicActionMAPTeam();
+		
+		/* Create the SimulationEngine */
+		SimulationEngine se = new SimulationEngine(teams);
+		//System.out.println("DISTURBANCE\tAD-ACTION-MAP\tHELPER-INIT\tBASIC-ACTION");
+		/* The experiments loop */
+		
+			/* Set the experiment-wide parameters: */
+			/* teams-wide, SimulationEngine, etc params */			
+		
+		for (int expx=1;expx<2;expx++)
+		{
+			/* vary the disturbance as the x-axis: */  
+			for (int expy=-10;expy<1;expy++)
+			{	
+				if(experiment == 1)
+				{
+					Team.unicastCost = 1;
+					Agent.calculationCost = 1;			
+					SimulationEngine.disturbanceLevel = 0.1;
+					TeamTask.initResCoef = 120;
+					AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 24;
+					AdvActionMAPAgent.WLL = expy * 0.05;
+					HelperInitActionMAPAgent.WHH = 0.1;
+				}
+				else if(experiment == 2)
+				{
+					Team.unicastCost = 3;
+					Agent.calculationCost = 2;			
+					SimulationEngine.disturbanceLevel = 0.1;
+					TeamTask.initResCoef = 160;
+					AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 24;
+					AdvActionMAPAgent.WLL = expy * 0.05;
+					HelperInitActionMAPAgent.WHH = 0.1;
+				}
+				else
+				{
+					Team.unicastCost = 5;
+					Agent.calculationCost = 2;			
+					SimulationEngine.disturbanceLevel = 0.2;
+					TeamTask.initResCoef = 200;
+					AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 24;
+					AdvActionMAPAgent.WLL = expy * 0.05;
+					HelperInitActionMAPAgent.WHH = 0.1;
+				}
+				//HelperInitActionMAPAgent.WHL = expx * 0.05;
+				
+				/*if(HelperInitActionMAPAgent.WHL >= HelperInitActionMAPAgent.WHH)
+				{
+					System.out.print(HelperInitActionMAPAgent.WHL);
+					System.out.print(","+HelperInitActionMAPAgent.WHH);
+					System.out.println(",15000,10000,15000");
+					continue;
+				}*/
+				Team.broadcastCost = Team.unicastCost * (Team.teamSize-1);
+				TeamTask.helpOverhead = 20;			
+				TeamTask.cellReward = 100;
+				TeamTask.achievementReward = 2000;
+				TeamTask.assignmentOverhead = 0;
+				
+				AdvActionMAPAgent.requestThreshold = 489;
+				AdvActionMAPAgent.lowCostThreshold = 50;
+				AdvActionMAPAgent.importanceVersion = HelperInitActionMAPAgent.importanceVersion = impVersion;
+				
+				HelperInitActionMAPAgent.requestThreshold = 289;
+				HelperInitActionMAPAgent.EPSILON = 0.1;
+				
+				BasicActionMAPAgent.requestThreshold = 489;
+				
+				RAAgent.EPSILON = 0.2;
+				RAAgent.WREASSIGN = 0.3;
+				RAAgent.WREASSIGNREQ = 1.0;
+
+				/* Initialize and run the experiment */
+				se.initializeExperiment(numberOfRuns);			
+				int[] teamScores = se.runExperiment();
+
+
+				/* Print the results */
+				DecimalFormat df = new DecimalFormat("0.00");
+				//System.out.print(HelperInitActionMAPAgent.WHH);
+				strMsg = AdvActionMAPAgent.WLL + "";
+				//System.out.print(","+HelperInitActionMAPAgent.WHL);
+				for (int i=0;i<teams.length;i++)
+					strMsg += "," + teamScores[i];
+				//System.out.printf(",%d", teamScores[i]);
+				//System.out.println("");
+				//strMsg += "\n";
+				
+				try {
+				    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("outfilename.txt", true)));
+				    out.println(strMsg);
+				    out.close();
+				} catch (IOException e) {
+				    //oh noes!
+					System.err.println("Error writing file.." + strMsg);
+				}
+			}
+		}
+	}
+
+/*
+	public static void sendEmail()
+	{
+		try
+		{
+			String host = "smtp.gmail.com";
+		    String from = "denish.smith77@gmail.com";
+		    String pass = "smith1234";
+		    Properties props = System.getProperties();
+		    props.put("mail.smtp.starttls.enable", "true"); // added this line
+		    props.put("mail.smtp.host", host);
+		    props.put("mail.smtp.user", from);
+		    props.put("mail.smtp.password", pass);
+		    props.put("mail.smtp.port", "587");
+		    props.put("mail.smtp.auth", "true");
+	
+		    String[] to = {"mumbaiw@unbc.ca"}; // added this line
+	
+		    Session session = Session.getDefaultInstance(props, null);
+		    MimeMessage message = new MimeMessage(session);
+		    message.setFrom(new InternetAddress(from));
+	
+		    InternetAddress[] toAddress = new InternetAddress[to.length];
+	
+		    // To get the array of addresses
+		    for( int i=0; i < to.length; i++ ) { // changed from a while loop
+		        toAddress[i] = new InternetAddress(to[i]);
+		    }
+		    System.out.println(Message.RecipientType.TO);
+	
+		    for( int i=0; i < toAddress.length; i++) { // changed from a while loop
+		        message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+		    }
+		    message.setSubject("Test email");
+		    message.setText("Welcome to JavaMail");
+		    Transport transport = session.getTransport("smtp");
+		    transport.connect(host, from, pass);
+		    transport.sendMessage(message, message.getAllRecipients());
+		    transport.close();
+		}
+		catch (Exception ex)
+		{
+			System.err.println("Error : " + ex.getMessage());
+		}
+	}
+	*/
+}
---
+++ src/experiments/wellbeing/WellBeingTester.java
@@ -0,0 +1,408 @@
+package experiments.wellbeing;
+
+import java.io.*;
+import java.util.Date;
+
+import massim.Agent;
+import massim.SimulationEngine;
+import massim.Team;
+import massim.TeamTask;
+import massim.agents.advancedactionmap.AdvActionMAPAgent;
+import massim.agents.advancedactionmap.AdvActionMAPRepAgent;
+import massim.agents.advancedactionmap.AdvActionMapTeam;
+import massim.agents.basicactionmap.BasicActionMAPAgent;
+import massim.agents.basicactionmap.BasicActionMAPTeam;
+import massim.agents.helperinitactionmap.HelperInitActionMAPAgent;
+import massim.agents.helperinitactionmap.HelperInitActionMAPTeam;
+import massim.agents.nohelp.NoHelpTeam;
+import massim.agents.reassignment.RAAgent;
+
+
+/**
+ * This is an experiment for testing various MAPs
+ * over Init Resource Coeff, Disturbance, No of Runs, Unicast cost & other parameters
+ * 
+ *   
+ * @author Denish M
+ *
+ */
+public class WellBeingTester {
+
+	static String strMsg = "";
+	public static void main(String[] args) {
+			try {
+				runSimulation1(10000, 2);
+				//Check no. of arguments
+				if(args.length < 2 || Integer.parseInt(args[0]) < 1)
+				{
+					System.out.println("Insufficient arguments or first argument is invalid. Please pass value between 1 and 4.");
+					return;
+				}
+				//run specific experiment based on input
+				if(Integer.parseInt(args[0]) == 1)
+				{
+					System.out.println(new Date());
+					runSimulation1(Integer.parseInt(args[1]), 2);
+					System.out.println(new Date());
+				}
+				else if(Integer.parseInt(args[0]) == 2)
+				{
+					System.out.println(new Date());
+					runSimulation2(Integer.parseInt(args[1]), 2);
+					System.out.println(new Date());
+				}
+				else if(Integer.parseInt(args[0]) == 3)
+				{
+					System.out.println(new Date());
+					runSimulation3(Integer.parseInt(args[1]), 2);
+					System.out.println(new Date());
+				}
+				else if(Integer.parseInt(args[0]) == 4)
+				{
+					System.out.println(new Date());
+					runSimulation4(Integer.parseInt(args[1]), 2);
+					System.out.println(new Date());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+	/*
+	 * Custom simulation number one
+	 */
+	public static void runSimulation1(int numberOfRuns, int impVersion) throws Exception {
+		if(numberOfRuns < 1)
+			throw new Exception("numberOfRuns is invalid!");
+		
+		SimulationEngine.colorRange = 
+			new int[] {0, 1, 2, 3, 4, 5};
+		SimulationEngine.numOfColors =  
+			SimulationEngine.colorRange.length;
+		SimulationEngine.actionCostsRange = 
+		new int[] {10, 40, 150, 200, 250, 300, 350, 500};	
+	
+		/* Create the teams involved in the simulation */
+		Team.teamSize = 8;
+		Team[] teams = new Team[8];
+		teams[0] = new AdvActionMapTeam();
+		teams[1] = new HelperInitActionMAPTeam();
+		teams[2] = new BasicActionMAPTeam();
+		teams[3] = new NoHelpTeam();
+
+		teams[4] = new AdvActionMapTeam();
+		teams[5] = new HelperInitActionMAPTeam();
+		teams[6] = new BasicActionMAPTeam();
+		teams[7] = new NoHelpTeam();
+		
+		//set if calculate remaining resources in rewards
+		teams[4].remainingResInRewards = teams[5].remainingResInRewards 
+				= teams[6].remainingResInRewards = teams[7].remainingResInRewards = true;
+				
+		/* Create the SimulationEngine */
+		SimulationEngine se = new SimulationEngine(teams);
+		logText("IMPFACT,INITRES,AD-ACTION-MAP,HELPER-INIT,BASIC-ACTION,NO-HELP,AD-ACTION-MAP-RR,HELPER-INIT-RR,BASIC-ACTION-RR,NO-HELP-RR",1);
+		
+		//loop different values of variables
+		for (int expx=1;expx<21;expx++)
+		{  
+			for (int expy=10;expy<21;expy++)
+			{	
+				Team.unicastCost = 1;
+				Agent.calculationCost = 1;			
+				SimulationEngine.disturbanceLevel = 0.2;
+				TeamTask.initResCoef = expy * 10;
+				AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = expx * 5;
+				AdvActionMAPAgent.WLL = -0.1;
+				HelperInitActionMAPAgent.WHH = 0.1;
+				Team.paramValues = AdvActionMAPAgent.impFactor + "," + TeamTask.initResCoef;
+				Team.dbgScores = false;
+				AdvActionMAPAgent.useTeamWellbeing = HelperInitActionMAPAgent.useTeamWellbeing = false;
+
+				Team.broadcastCost = Team.unicastCost * (Team.teamSize-1);
+				TeamTask.helpOverhead = 20;			
+				TeamTask.cellReward = 100;
+				TeamTask.achievementReward = 2000;
+				TeamTask.assignmentOverhead = 0;
+				
+				AdvActionMAPAgent.requestThreshold = 489;
+				AdvActionMAPAgent.lowCostThreshold = 50;
+				AdvActionMAPAgent.importanceVersion = HelperInitActionMAPAgent.importanceVersion = impVersion;
+				
+				HelperInitActionMAPAgent.requestThreshold = 299;
+				HelperInitActionMAPAgent.EPSILON = 0.1;
+				
+				BasicActionMAPAgent.requestThreshold = 489;
+				
+				RAAgent.EPSILON = 0.2;
+				RAAgent.WREASSIGN = 0.3;
+				RAAgent.WREASSIGNREQ = 1.0;
+
+				/* Initialize and run the experiment */
+				se.initializeExperiment(numberOfRuns);			
+				int[] teamScores = se.runExperiment();
+
+				/* Print the results */
+				strMsg =  Team.paramValues;
+				for (int i=0;i<teams.length;i++)
+					strMsg += "," + teamScores[i];
+				
+				logText(strMsg,1);
+			}
+		}		
+		logText("-----------------------------------------",1);
+	}
+	
+	/*
+	 * Custom simulation number two
+	 */
+	public static void runSimulation2(int initResCoeff, int impVersion) throws Exception {
+		int numberOfRuns = 35;
+		if(numberOfRuns < 1)
+			throw new Exception("numberOfRuns is invalid!");
+		
+		SimulationEngine.colorRange = 
+			new int[] {0, 1, 2, 3, 4, 5};
+		SimulationEngine.numOfColors =  
+			SimulationEngine.colorRange.length;
+		SimulationEngine.actionCostsRange = 
+		new int[] {10, 40, 150, 200, 250, 300, 350, 500};	
+	
+		/* Create the teams involved in the simulation */
+		Team.teamSize = 8;
+		Team[] teams = new Team[8];
+		teams[0] = new AdvActionMapTeam();
+		teams[1] = new HelperInitActionMAPTeam();
+		teams[2] = new BasicActionMAPTeam();
+		teams[3] = new NoHelpTeam();
+		
+		teams[4] = new AdvActionMapTeam();
+		teams[5] = new HelperInitActionMAPTeam();
+		teams[6] = new BasicActionMAPTeam();
+		teams[7] = new NoHelpTeam();
+		teams[4].remainingResInRewards = teams[5].remainingResInRewards 
+				= teams[6].remainingResInRewards = teams[7].remainingResInRewards = true;
+
+		
+		/* Create the SimulationEngine */
+		SimulationEngine se = new SimulationEngine(teams);
+		logText("UNICAST,INITRES,AD-ACTION-MAP,HELPER-INIT,BASIC-ACTION,NO-HELP,AD-ACTION-MAP-RES,HELPER-INIT-RES,BASIC-ACTION-RES,NO-HELP-RES",initResCoeff);
+		for (int expx=0;expx<11;expx++)
+		{  
+			for (int expy=10;expy< 11;expy++)
+			{	
+				Team.unicastCost = 2 * expx +1;
+				Agent.calculationCost = 1;			
+				SimulationEngine.disturbanceLevel = 0.2;
+				TeamTask.initResCoef = initResCoeff;//expy * 10;
+				AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 6;
+				AdvActionMAPAgent.WLL = -0.1;
+				HelperInitActionMAPAgent.WHH = 0.1;
+				Team.paramValues = Team.unicastCost + "";
+				Team.dbgScores = true;
+				AdvActionMAPAgent.useTeamWellbeing = HelperInitActionMAPAgent.useTeamWellbeing = false;
+				
+				Team.broadcastCost = Team.unicastCost * (Team.teamSize-1);
+				TeamTask.helpOverhead = 20;			
+				TeamTask.cellReward = 100;
+				TeamTask.achievementReward = 2000;
+				TeamTask.assignmentOverhead = 0;
+				
+				AdvActionMAPAgent.requestThreshold = 489;
+				AdvActionMAPAgent.lowCostThreshold = 50;
+				AdvActionMAPAgent.importanceVersion = HelperInitActionMAPAgent.importanceVersion = impVersion;
+				
+				HelperInitActionMAPAgent.requestThreshold = 299;
+				HelperInitActionMAPAgent.EPSILON = 0.1;
+				
+				BasicActionMAPAgent.requestThreshold = 489;
+				
+				RAAgent.EPSILON = 0.2;
+				RAAgent.WREASSIGN = 0.3;
+				RAAgent.WREASSIGNREQ = 1.0;
+
+				/* Initialize and run the experiment */
+				se.initializeExperiment(numberOfRuns);			
+				int[] teamScores = se.runExperiment();
+
+
+				/* Print the results */
+				strMsg = Team.paramValues;
+				for (int i=0;i<teams.length;i++)
+					strMsg += "," + teamScores[i];
+				
+				logText(strMsg,initResCoeff);
+			}
+		}
+		
+		logText("-----------------------------------------",initResCoeff);
+	}
+	
+	/*
+	 * Custom simulation number three
+	 */
+	public static void runSimulation3(int numberOfRuns, int impVersion) throws Exception {
+		if(numberOfRuns < 1)
+			throw new Exception("numberOfRuns is invalid!");
+		
+		SimulationEngine.colorRange = 
+			new int[] {0, 1, 2, 3, 4, 5};
+		SimulationEngine.numOfColors =  
+			SimulationEngine.colorRange.length;
+		SimulationEngine.actionCostsRange = 
+		new int[] {10, 40, 150, 200, 250, 300, 350, 500};	
+	
+		/* Create the teams involved in the simulation */
+		Team.teamSize = 8;
+		Team[] teams = new Team[4];
+		teams[0] = new AdvActionMapTeam();
+		teams[1] = new HelperInitActionMAPTeam();
+		teams[2] = new BasicActionMAPTeam();
+		teams[3] = new NoHelpTeam();
+		
+		/* Create the SimulationEngine */
+		SimulationEngine se = new SimulationEngine(teams);
+		logText("WHH,AD-ACTION-MAP,HELPER-INIT,BASIC-ACTION,NO-HELP",3);
+		for (int expx=1;expx<2;expx++)
+		{
+			for (int expy=0;expy<13;expy++)
+			{	
+				Team.unicastCost = 4;
+				Agent.calculationCost = 2;			
+				SimulationEngine.disturbanceLevel = 0.1;
+				TeamTask.initResCoef = 160;
+				AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 24;
+				AdvActionMAPAgent.WLL = -0.1;
+				HelperInitActionMAPAgent.WHH = expy * 0.05;
+				Team.paramValues = HelperInitActionMAPAgent.WHH + "";
+				Team.dbgScores = false;
+				AdvActionMAPAgent.useTeamWellbeing = HelperInitActionMAPAgent.useTeamWellbeing = false;
+
+				Team.broadcastCost = Team.unicastCost * (Team.teamSize-1);
+				TeamTask.helpOverhead = 20;			
+				TeamTask.cellReward = 100;
+				TeamTask.achievementReward = 2000;
+				TeamTask.assignmentOverhead = 0;
+				
+				AdvActionMAPAgent.requestThreshold = 489;
+				AdvActionMAPAgent.lowCostThreshold = 50;
+				AdvActionMAPAgent.importanceVersion = HelperInitActionMAPAgent.importanceVersion = impVersion;
+				
+				HelperInitActionMAPAgent.requestThreshold = 299;
+				HelperInitActionMAPAgent.EPSILON = 0.1;
+				
+				BasicActionMAPAgent.requestThreshold = 489;
+				
+				RAAgent.EPSILON = 0.2;
+				RAAgent.WREASSIGN = 0.3;
+				RAAgent.WREASSIGNREQ = 1.0;
+
+				/* Initialize and run the experiment */
+				se.initializeExperiment(numberOfRuns);			
+				int[] teamScores = se.runExperiment();
+
+
+				/* Print the results */
+				strMsg =  Team.paramValues;
+				//strMsg += TeamTask.initResCoef;
+				for (int i=0;i<teams.length;i++)
+					strMsg += "," + teamScores[i];
+				
+				logText(strMsg,3);
+			}
+		}
+		logText("-----------------------------------------",3);
+	}
+	
+	/*
+	 * Custom simulation number four
+	 */
+	public static void runSimulation4(int numberOfRuns, int impVersion) throws Exception {
+	//	if(wbVersion < 1 || wbVersion > 2)
+		//	throw new Exception("Wellbeing value is invalid!");
+		if(numberOfRuns < 1)
+			throw new Exception("numberOfRuns is invalid!");
+		
+		SimulationEngine.colorRange = 
+			new int[] {0, 1, 2, 3, 4, 5};
+		SimulationEngine.numOfColors =  
+			SimulationEngine.colorRange.length;
+		SimulationEngine.actionCostsRange = 
+		new int[] {10, 40, 150, 200, 250, 300, 350, 500};	
+	
+		/* Create the teams involved in the simulation */
+		Team.teamSize = 8;
+		Team[] teams = new Team[4];
+		teams[0] = new AdvActionMapTeam();
+		teams[1] = new HelperInitActionMAPTeam();
+		teams[2] = new BasicActionMAPTeam();
+		teams[3] = new NoHelpTeam();
+		
+		/* Create the SimulationEngine */
+		SimulationEngine se = new SimulationEngine(teams);
+		logText("WLL,AD-ACTION-MAP,HELPER-INIT,BASIC-ACTION,NO-HELP",4);
+		for (int expx=1;expx<2;expx++)
+		{
+			/* vary the disturbance as the x-axis: */  
+			for (int expy=-10;expy<1;expy++)
+			{	
+				Team.unicastCost = 3;
+				Agent.calculationCost = 2;			
+				SimulationEngine.disturbanceLevel = 0.1;
+				TeamTask.initResCoef = 160;
+				AdvActionMAPAgent.impFactor = HelperInitActionMAPAgent.impFactor = 24;
+				AdvActionMAPAgent.WLL = expy * 0.05;
+				HelperInitActionMAPAgent.WHH = 0.1;
+				Team.paramValues = AdvActionMAPAgent.WLL + "";
+				Team.dbgScores = false;
+				AdvActionMAPAgent.useTeamWellbeing = HelperInitActionMAPAgent.useTeamWellbeing = false;
+
+				Team.broadcastCost = Team.unicastCost * (Team.teamSize-1);
+				TeamTask.helpOverhead = 20;			
+				TeamTask.cellReward = 100;
+				TeamTask.achievementReward = 2000;
+				TeamTask.assignmentOverhead = 0;
+				
+				AdvActionMAPAgent.requestThreshold = 489;
+				AdvActionMAPAgent.lowCostThreshold = 50;
+				AdvActionMAPAgent.importanceVersion = HelperInitActionMAPAgent.importanceVersion = impVersion;
+				
+				HelperInitActionMAPAgent.requestThreshold = 299;
+				HelperInitActionMAPAgent.EPSILON = 0.1;
+				
+				BasicActionMAPAgent.requestThreshold = 489;
+				
+				RAAgent.EPSILON = 0.2;
+				RAAgent.WREASSIGN = 0.3;
+				RAAgent.WREASSIGNREQ = 1.0;
+
+				/* Initialize and run the experiment */
+				se.initializeExperiment(numberOfRuns);			
+				int[] teamScores = se.runExperiment();
+
+
+				/* Print the results */
+				strMsg =  Team.paramValues;
+				for (int i=0;i<teams.length;i++)
+					strMsg += "," + teamScores[i];
+				
+				logText(strMsg,4);
+			}
+		}
+		logText("-----------------------------------------",4);
+	}
+	
+	/*
+	 * Log message in text file specific to the experiment
+	 */
+	public static void logText(String strMsg, int expID)
+	{
+		try {
+			System.out.println(strMsg);
+		    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("outfilename" + expID + ".txt", true)));
+		    out.println(strMsg);
+		    out.close();
+		} catch (IOException e) {
+			System.err.println("Error writing file.." + strMsg);
+		}
+	}
+}
